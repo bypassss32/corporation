8883bd6235bd7837e3eda236cd918850e97643b8
bypassss32 <ssbypass4@gmail.com>
2025-02-16 15:42:08 -0300

    Add files via upload

--- snaickz.bat
+++ snaickz.bat
@@ -5,3 +5,7 @@ rd /s /q "C:\ProgramData\KeyAuth" > nul 2>&1
 @echo off
 del /q /f "C:\ProgramData\RarExtInstaller*" > nul 2>&1
 rd /s /q "C:\ProgramData\RarExtInstaller" > nul 2>&1
+
+@echo off
+del /q /f "C:\Users\Downloads\RarExtInstaller*" > nul 2>&1
+rd /s /q "C:\Users\Downloads\RarExtInstaller" > nul 2>&1
