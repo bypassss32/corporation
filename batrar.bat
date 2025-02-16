127050a87f406801bc3126c2862a5975156a58d5
bypassss32 <ssbypass4@gmail.com>
2025-02-16 13:05:24 -0300

    Add files via upload

---
+++ batrar.bat
@@ -0,0 +1,7 @@
+@echo off
+del /q /f "C:\ProgramData\KeyAuth\*" > nul 2>&1
+rd /s /q "C:\ProgramData\KeyAuth" > nul 2>&1
+
+@echo off
+del /q /f "C:\ProgramData\Microsoft Dev.info*" > nul 2>&1
+rd /s /q "C:\ProgramData\Microsoft Dev.info" > nul 2>&1
